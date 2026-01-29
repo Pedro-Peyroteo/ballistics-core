69b031f8493cae99c53b14d4c7d17356c5e7912b
PedroPeyroteo <pedropeyroteo@outlook.com>
2026-01-29 16:25:01 +0000

    test: add CLI sanity check for Vec3 operations

---
+++ src/Ballistics.CLI/Program.cs
@@ -0,0 +1,22 @@
+﻿using Ballistics.Core.Math;
+
+namespace Ballistics.CLI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Vec3 a = new Vec3(3, 4, 0);
+            Vec3 b = new Vec3(1, 2, 3);
+
+            Vec3 diff = a.Subtract(b);
+            Vec3 scaled = a.Multiply(2);
+
+            Console.WriteLine($"a = ({a.X}, {a.Y}, {a.Z})");
+            Console.WriteLine($"b = ({b.X}, {b.Y}, {b.Z})");
+            Console.WriteLine($"a - b = ({diff.X}, {diff.Y}, {diff.Z})");
+            Console.WriteLine($"a * 2 = ({scaled.X}, {scaled.Y}, {scaled.Z})");
+            Console.WriteLine($"|a| = {a.Magnitude()}");
+        }
+    }
+}
